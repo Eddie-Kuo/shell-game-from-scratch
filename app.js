7399687ebd3952febc947ad062bdb765d0464879
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-07-26 20:10:28 -0500

    add a function to let the user guess which card is the lady and display the score at the bottom

--- app.js
+++ app.js
@@ -1,8 +1,81 @@
 // import functions and grab DOM elements
 
+const leftCardEl = document.getElementById('left-card');
+const middleCardEl = document.getElementById('middle-card');
+const rightCardEl = document.getElementById('right-card');
+
+const leftButtEl = document.getElementById('left-button');
+const middleButtEl = document.getElementById('middle-button');
+const rightButtEl = document.getElementById('right-button');
+
+const winsEl = document.getElementById('wins');
+const lossesEl = document.getElementById('losses');
+const attemptsEl = document.getElementById('attempts');
+
 // let state
 
+const hidingPlaces = ['left', 'middle', 'right'];
+
+let correctGuesses = 0;
+let incorrectGuesses = 0;
+let totalAttempts = 0;
+
 // set event listeners 
+
+
+
+
+
+
+
+
   // get user input
+
+
+
+
+
+
+
+
+
+
+
+
+
   // use user input to update state 
   // update DOM to reflect the new state
+
+
+
+
+
+leftButtEl.addEventListener('click', () => {
+    let answer = getRandomItem(hidingPlaces);
+    handleGuess(answer, 'left');
+});
+
+middleButtEl.addEventListener('click', () => {
+    let answer = getRandomItem(hidingPlaces);
+    handleGuess(answer, 'middle');
+});
+
+rightButtEl.addEventListener('click', () => {
+    let answer = getRandomItem(hidingPlaces);
+    handleGuess(answer, 'right');
+});
+
+function getRandomItem(array) {
+    const randomCard = Math.floor(Math.random() * array.length);
+    return array[randomCard];
+}
+
+function handleGuess(correctSpot, userGuess) {
+    if (correctSpot === userGuess) {
+        winsEl.textContent = correctGuesses++;
+        attemptsEl.textContent = totalAttempts++;
+    } else {
+        lossesEl.textContent = incorrectGuesses++;
+        attemptsEl.textContent = totalAttempts++;
+    }
+}
