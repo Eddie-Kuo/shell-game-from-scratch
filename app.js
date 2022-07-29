71730accfe919ca6e03c79ff13f98aa23055324a
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-07-29 13:31:41 -0500

    add functionality to the reset buttons and reveal cards based on user choice

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const winsEl = document.getElementById('wins');
 const lossesEl = document.getElementById('losses');
 const attemptsEl = document.getElementById('attempts');
 
+const resetButt = document.getElementById('reset-button');
+const resetScore = document.getElementById('score-reset');
+
 // let state
 
 const hidingPlaces = ['left', 'middle', 'right'];
@@ -21,6 +24,71 @@ let incorrectGuesses = 0;
 let totalAttempts = 0;
 
 // set event listeners 
+
+leftButtEl.addEventListener('click', () => {
+    let answer = getRandomItem(hidingPlaces);
+    handleGuess(answer, 'left');
+});
+
+middleButtEl.addEventListener('click', () => {
+    let answer = getRandomItem(hidingPlaces);
+    handleGuess(answer, 'middle');
+});
+
+rightButtEl.addEventListener('click', () => {
+    let answer = getRandomItem(hidingPlaces);
+    handleGuess(answer, 'right');
+});
+
+resetButt.addEventListener('click', () => {
+    leftCardEl.src = './assets/card-back.png';
+    middleCardEl.src = './assets/card-back.png';
+    rightCardEl.src = './assets/card-back.png';
+});
+
+resetScore.addEventListener('click', () => {
+    winsEl.textContent = 0;
+    lossesEl.textContent = 0;
+    attemptsEl.textContent = 0;
+    correctGuesses = 0;
+    incorrectGuesses = 0;
+    totalAttempts = 0;
+});
+
+
+
+function getRandomItem(array) {
+    const randomCard = Math.floor(Math.random() * array.length);
+    return array[randomCard];
+}
+
+function handleGuess(correctSpot, userGuess) {
+    if (correctSpot === userGuess) {
+        correctGuesses++;
+        totalAttempts++;
+        winsEl.textContent = correctGuesses;
+        attemptsEl.textContent = totalAttempts;
+    } else {
+        incorrectGuesses++;
+        totalAttempts++;
+        lossesEl.textContent = incorrectGuesses;
+        attemptsEl.textContent = totalAttempts;
+    }
+
+    if (correctSpot === 'left') {
+        leftCardEl.src = './assets/queen.png';
+        middleCardEl.src = './assets/jack.png';
+        rightCardEl.src = './assets/jack.png';
+    } else if (correctSpot === 'middle') {
+        leftCardEl.src = './assets/jack.png';
+        middleCardEl.src = './assets/queen.png';
+        rightCardEl.src = './assets/jack.png';
+    } else if (correctSpot === 'right') {
+        leftCardEl.src = './assets/jack.png';
+        middleCardEl.src = './assets/jack.png';
+        rightCardEl.src = './assets/queen.png';
+    }
+}
 
 
 
@@ -48,37 +116,3 @@ let totalAttempts = 0;
 
 
 
-
-leftButtEl.addEventListener('click', () => {
-    let answer = getRandomItem(hidingPlaces);
-    handleGuess(answer, 'left');
-});
-
-middleButtEl.addEventListener('click', () => {
-    let answer = getRandomItem(hidingPlaces);
-    handleGuess(answer, 'middle');
-});
-
-rightButtEl.addEventListener('click', () => {
-    let answer = getRandomItem(hidingPlaces);
-    handleGuess(answer, 'right');
-});
-
-function getRandomItem(array) {
-    const randomCard = Math.floor(Math.random() * array.length);
-    return array[randomCard];
-}
-
-function handleGuess(correctSpot, userGuess) {
-    if (correctSpot === userGuess) {
-        correctGuesses++;
-        totalAttempts++;
-        winsEl.textContent = correctGuesses;
-        attemptsEl.textContent = totalAttempts;
-    } else {
-        incorrectGuesses++;
-        totalAttempts++;
-        lossesEl.textContent = incorrectGuesses;
-        attemptsEl.textContent = totalAttempts;
-    }
-}
